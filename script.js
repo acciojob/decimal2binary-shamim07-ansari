df90dc1bd81c8d9751d7e72ccf236ab97ac60d99
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-15 18:18:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+	let ans = "";
+	while(num != 0) {
+		let bit = num & 1;
+		ans += bit;
+		num = num >> 1;
+	}
+	let result = "";
+	for(let i=ans.length-1; i>=0; i--) {
+	    result += ans.charAt(i);
+	}
+	return result;
 }
 
 window.decimalToBinary = decimalToBinary;
